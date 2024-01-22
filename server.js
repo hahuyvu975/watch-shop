8c7005a202716e2300326a2b09cffc3c05d843c9
hahuyvu975 <hahuyvu975@gmail.com>
2024-01-22 12:06:13 +0700

    Connect mongodb

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const express = require('express')
-const color = require('colors')
+import express from "express";
+import color from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+//configure env'
+dotenv.config();
+
+//database config
+connectDB();
+
 //rest object
 const app = express();
 
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'))
+
 //rest api
-app.get('/', () => {
-    console.log('hello')
+app.get('/', (req,res) => {
+    res.send({message: 'Welcome hivu2024'})
 })
 
 //PORT
-const PORT = 8080
-
+const PORT = process.env.PORT
 //run listen
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`.bgCyan.white);
+    console.log(`Server runnin qg on ${PORT}`.bgCyan.white);
 })
