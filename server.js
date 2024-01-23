74b9cecc9995248963e0597498e1f4785a688c34
hahuyvu975 <hahuyvu975@gmail.com>
2024-01-23 14:24:48 +0700

    update server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import color from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routers/authRoute.js"
 
 //configure env'
 dotenv.config();
@@ -17,6 +18,8 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'))
 
+//routes
+app.use('/api/v1/auth', authRoutes);
 //rest api
 app.get('/', (req,res) => {
     res.send({message: 'Welcome hivu2024'})
