ee589c7f857f1e2f0474fa3c2d5f2ee7f5d007ee
hahuyvu975 <hahuyvu975@gmail.com>
2024-01-29 13:04:32 +0700

    fix UI page contact

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,29 +4,30 @@ import { BiSupport, BiMailSend, BiPhoneCall } from "react-icons/bi";
 const Contact = () => {
   return (
     <Layout>
-      <div className='row contactus'>
-        <div className='col-md-6'>
-          <img 
-          src='/images/contactus.jpeg'
-          alt='contactus'
-          style={{width: "100%"}}
-          />
-        </div>
-        <div className='col-md-6'>
-          <h1 className='bg-dark p-2 text-white text-center'>CONTACT US</h1>
-          <p className='text-justify mt-2'>
-            Any query and info about product feel free to call anytime we 24/7 vaialible
-          </p>
-          <p className='mt-3'>
-            <BiMailSend /> : www.hahuyvu975@watchshop.com
-          </p>
-          <p className='mt-3'>
-            <BiPhoneCall /> : 0901 356 975
-          </p>
-          <p className='mt-3'>
-            <BiSupport /> : 1800-356-975 (toll free)
-          </p>
-
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 order-md-1">
+            <img
+              src="/images/contactus.jpeg"
+              alt="Contact Us"
+              className="img-fluid"
+            />
+          </div>
+          <div className="col-md-6 order-md-2">
+            <h2>Contact Us</h2>
+            <p className='text-justify mt-2'>
+              Any query and info about product feel free to call anytime we 24/7 vaialible
+            </p>
+            <p className='mt-3'>
+              <BiMailSend /> : www.hahuyvu975@watchshop.com
+            </p>
+            <p className='mt-3'>
+              <BiPhoneCall /> : 0901 356 975
+            </p>
+            <p className='mt-3'>
+              <BiSupport /> : 1800-356-975 (toll free)
+            </p>
+          </div>
         </div>
       </div>
     </Layout>
@@ -34,3 +35,4 @@ const Contact = () => {
 }
 
 export default Contact
+
