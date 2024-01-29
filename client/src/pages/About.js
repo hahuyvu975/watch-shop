6a898710370a7e34b050263ee0f4365c90c85285
hahuyvu975 <hahuyvu975@gmail.com>
2024-01-29 13:03:54 +0700

    fix UI page about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,23 +4,30 @@ import Layout from '../components/Layout/Layout'
 const About = () => {
   return (
     <Layout>
-      <div className='row aboutus'>
-        <div className='col-md-6 mt-5 d-flex align-items-center'>
-          <img
-            src='/images/aboutus.jpeg'
-            alt='aboutus'
-            style={{ width: "90%", margin: "auto"}}
-          />
-        </div>
-        <div className='col-md-6 d-flex align-items-center'>
-
-          <p className='text-center'>
-            Welcome to Watch Shop, where time meets elegance. We are passionate about bringing you a curated collection of exquisite timepieces that blend precision with style.
-
-            At Watch Shop, we believe that a watch is more than just a timekeeping device; it's a reflection of one's personality and a symbol of timeless sophistication. Our mission is to provide you with a unique selection of watches that not only tell time but also tell your story.
-          </p>
-
-
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 order-md-1">
+            <img
+              src="/images/aboutus.jpeg"
+              alt="About Us"
+              className="img-fluid"
+            />
+          </div>
+          <div className="col-md-6 order-md-2">
+            <h2>About Us</h2>
+            <p>
+              Watch Shop is more than just a timepiece retailer; we are curators of elegance and sophistication. Established with a passion for blending precision with style, our goal is to offer a meticulously curated collection of exquisite timepieces that transcend mere functionality.
+            </p>
+            <p>
+              At Watch Shop, we believe that a watch is more than a device that tells time; it is an expression of one's individuality and a symbol of timeless sophistication. Our mission is to provide you with a unique selection of watches that not only keep you on schedule but also tell your unique story.
+            </p>
+            <p>
+              As a destination for watch enthusiasts, Watch Shop is committed to delivering not just products but experiences. Our knowledgeable and passionate team is dedicated to assisting you in finding the watch that resonates with your style and complements your personality.
+            </p>
+            <p>
+              Welcome to Watch Shop, where every tick is a testament to your individuality and a celebration of timeless elegance
+            </p>
+          </div>
         </div>
       </div>
     </Layout>
