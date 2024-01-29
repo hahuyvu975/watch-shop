6259f4d3cfed101505ad1e1517ef70660280cf5a
hahuyvu975 <hahuyvu975@gmail.com>
2024-01-29 13:39:13 +0700

    init props Layout

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import { Helmet } from 'react-helmet';
 
-const Layout = ({children}) => {
+const Layout = ({ children, title, description, keywords, author }) => {
     return (
         <div>
-               <Header/>
-                   <main style={{minHeight: '70vh'}}>
-                       {children}
-                   </main>
-               <Footer/>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <meta name='description' content={description} />
+                <meta name='keywords' content={keywords} />
+                <meta name='author' content={author} />
+                <title>{title}</title>
+            </Helmet>
+            <Header />
+            <main style={{ minHeight: '70vh' }}>
+                {children}
+            </main>
+            <Footer />
         </div>
 
-    )
+    );
+};
+
+Layout.defaultProps = {
+    title: 'Watch Shop - Shop Now',
+    description: 'Discover a curated collection of exquisite timepieces at Watch Shop',
+    keywords: 'watches, luxury watches, timepieces, watch shop',
+    author: 'huyvu'
 }
 
 export default Layout
