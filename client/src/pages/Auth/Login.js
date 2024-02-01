b1e21da8315f5b1804a8b139c7522419cd1152bf
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-01 12:32:49 +0700

    setAuth get token&user after login

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,10 +1,74 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import React, { useState } from 'react';
+import Layout from '../../components/Layout/Layout';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
+import { toast } from 'react-hot-toast';
+import '../../styles/AuthStyles.css'
+import { useAuth } from '../../context/auth.js';
 
 const Login = () => {
+    const [email, setEmail] = useState('user1@gmail.com');
+    const [password, setPassword] = useState('123456');
+    const [auth, setAuth] = useAuth();
+
+    const navigate = useNavigate();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(`/api/v1/auth/login`, {
+                email,
+                password,
+            });
+            if (res && res.data.success) {
+                toast.success(res.data.message);
+                setAuth({
+                    ...auth,
+                    user: res.data.user,
+                    token: res.data.token
+                });
+                localStorage.setItem("auth", JSON.stringify(res.data.token))
+                setTimeout(() => navigate('/'), 1000);
+            } else {
+                toast.error(res.data.message);
+                console.log('false')
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error('Register failed');
+        }
+    }
     return (
         <Layout title={"Login - Watch Shop"}>
-            <h1>Login</h1>
+            <div className='login-bg'>
+                <div className='form-container'>
+                    <form onSubmit={handleSubmit}>
+                        <h4 className='title'>LOGIN FORM</h4>
+                        <div className="mb-3">
+                            <label htmlFor="InputEmail">Email</label>
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="form-control"
+                                id="InputEmail"
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="InputPassword">Password</label>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="form-control"
+                                controlid="InputPassword"
+                                required
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Login</button>
+                    </form>
+                </div>
+            </div>
         </Layout>
     )
 }
