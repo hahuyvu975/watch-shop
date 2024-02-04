68e119f1dd1de0db9d6037bd18bdaed2da4495a5
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-04 14:29:51 +0700

    init dashboard user

---
+++ client/src/pages/user/DashBoard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const DashBoard = () => {
+    return (
+        <Layout title={'DashBoard - Watch Shop'}>
+            <h1>DashBoard Page</h1>
+        </Layout>
+    )
+}
+
+export default DashBoard;
