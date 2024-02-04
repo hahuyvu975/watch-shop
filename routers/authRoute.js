e4a0459b474b6b13a874fe7a3d1000f071d43c91
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-04 22:33:56 +0700

    create router forgot-password

--- routers/authRoute.js
+++ routers/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerController, loginController, testController } from '../controllers/authController.js'
+import { registerController, loginController, testController, forgotPasswordController } from '../controllers/authController.js'
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
 
 //route object
@@ -14,6 +14,10 @@ router.post('/login', loginController);
 
 router.get('/test', requireSignIn, isAdmin, testController);
 
+// Forgot Passowrd || PORT
+router.post('/forgot-password', forgotPasswordController);
+
+//protected route auth
 router.get('/user-auth', requireSignIn, (req, res) => {
     res.status(200).send({
         message: true
