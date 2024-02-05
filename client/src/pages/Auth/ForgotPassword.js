b1e64d1dd249e9c0c2de024d2466fed50ed342a5
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-05 15:54:30 +0700

    fix handChange

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Layout from '../../components/Layout/Layout'
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import axios from 'axios';
 import '../../styles/AuthStyles.css'
@@ -11,7 +11,6 @@ const ForgotPassword = () => {
     const [newPassword, setNewPassword] = useState('');
 
     const navigate = useNavigate();
-    const location = useLocation();
 
     const handleChange = async (e) => {
         e.preventDefault();
@@ -21,60 +20,61 @@ const ForgotPassword = () => {
                 answer,
                 newPassword
             });
-            console.log(res.data);
+
             if (res && res.data.success) {
                 toast.success(res.data.message);
-               
-                navigate(location.state || '/');
+                setTimeout(() => {
+                    navigate('/login');
+                }, 1500);
             } else {
                 toast.error(res.data.message);
             }
         } catch (error) {
             console.log(error);
-            toast.error('Register failed');
+            toast.error('Error reset password');
         }
     }
     return (
         <Layout title={"Forgot Password - Watch Shop"}>
             <div className='form-container'>
-                    <form onSubmit={handleChange} >
-                        <h4 className='title'>CHANGE PASSWORD FORM</h4>
-                        <div className="mb-3">
-                            <label htmlFor="InputEmail">Email</label>
-                            <input
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                className="form-control"
-                                id="InputEmail"
-                                required
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="InputAnswer">What is your favorite sports?</label>
-                            <input
-                                type="text"
-                                value={answer}
-                                onChange={(e) => setAnswer(e.target.value)}
-                                className="form-control"
-                                controlid="InputAnswer"
-                                required
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="InputNewPassword">New Password</label>
-                            <input
-                                type="password"
-                                value={newPassword}
-                                onChange={(e) => setNewPassword(e.target.value)}
-                                className="form-control"
-                                controlid="InputNewPassword"
-                                required
-                            />
-                        </div>
-                            <button type="submit" className="btn btn-primary">Change Password</button>
-                    </form>
-                </div>
+                <form onSubmit={handleChange} >
+                    <h4 className='title'>CHANGE PASSWORD FORM</h4>
+                    <div className="mb-3">
+                        <label htmlFor="InputEmail">Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="form-control"
+                            id="InputEmail"
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="InputAnswer">What is your favorite sports?</label>
+                        <input
+                            type="text"
+                            value={answer}
+                            onChange={(e) => setAnswer(e.target.value)}
+                            className="form-control"
+                            controlid="InputAnswer"
+                            required
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="InputNewPassword">New Password</label>
+                        <input
+                            type="password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            className="form-control"
+                            controlid="InputNewPassword"
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Change Password</button>
+                </form>
+            </div>
         </Layout>
     )
 }
