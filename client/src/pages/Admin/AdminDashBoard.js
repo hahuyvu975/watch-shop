993df660c8af62bc9fd0aa87ffc6fd797bc83571
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-07 07:22:23 +0700

    ui dashboard admin

--- client/src/pages/Admin/AdminDashBoard.js
+++ client/src/pages/Admin/AdminDashBoard.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import AdminMenu from '../../components/Layout/AdminMenu'
 
 const AdminDashBoard = () => {
   return (
-   <Layout>
-       <h1>Admin Dashboard</h1>
-   </Layout>
-  )
-}
+    <Layout>
+      <div className='container-fluid m-3 m-3'>
+        <div className='row'>
+          <div className='col-md-3'>
+            <AdminMenu />
+          </div>
+          <div className='col-md-9'>content</div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default AdminDashBoard
