578d0f43017922296a37e48df3ff118cf64fa1c6
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-07 16:41:44 +0700

    create routes for dashboard user

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import AdminRoute from './components/Routes/AdminRoute';
 import CreateCategory from './pages/Admin/CreateCategory';
 import CreateProduct from './pages/Admin/CreateProduct';
 import Users from './pages/Admin/Users';
+import Profile from './pages/User/Profile';
+import Orders from './pages/User/Orders';
 
 
 function App() {
@@ -27,15 +29,20 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
+
         <Route path='/dashboard' element={<PrivateRoute />}>
           <Route path='user' element={<DashBoard />} />
+          <Route path='user/profile' element={<Profile />} />
+          <Route path='user/orders' element={<Orders />} />
         </Route>
+
         <Route path='/dashboard' element={<AdminRoute />}>
           <Route path='admin' element={<AdminDashBoard />} />
           <Route path='admin/create-category' element={<CreateCategory />} />
           <Route path='admin/create-product' element={<CreateProduct />} />
           <Route path='admin/users' element={<Users />} />
         </Route>
+
         <Route path='/*' element={<Page404 />} />
       </Routes>
     </>
