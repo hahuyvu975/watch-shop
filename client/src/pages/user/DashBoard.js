e201783fb43db769c666155af6f0852c46203171
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-07 16:56:16 +0700

    dashboard show info user

--- client/src/pages/user/DashBoard.js
+++ client/src/pages/user/DashBoard.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import { useAuth } from '../../context/auth'
+import UserMenu from '../../components/Layout/UserMenu';
 
 const DashBoard = () => {
+    const [auth, setAuth] = useAuth();
     return (
-        <Layout title={'DashBoard - Watch Shop'}>
-            <h1>DashBoard Page</h1>
+        <Layout title={`DashBoard - ${auth?.user?.name}`}>
+            <div className='container-fluid m-3 p-3'>
+                <div className='row'>
+                    <div className='col-md-3'>
+                        <UserMenu />
+                    </div>
+                    <div className='col-md-9'>
+                        <div className='card w-75 p-3'>
+                            <h3>Name: {auth?.user?.name}</h3>
+                            <h3>Email: {auth?.user?.email}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
