8b1961528f563557d028aaa601c517c3795127c7
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-07 08:22:46 +0700

    UI and info admin in dashboard

--- client/src/pages/Admin/AdminDashBoard.js
+++ client/src/pages/Admin/AdminDashBoard.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import AdminMenu from '../../components/Layout/AdminMenu'
+import { useAuth } from '../../context/auth';
 
 const AdminDashBoard = () => {
+  const [auth, setAuth] = useAuth();
   return (
     <Layout>
       <div className='container-fluid m-3 m-3'>
@@ -10,7 +12,13 @@ const AdminDashBoard = () => {
           <div className='col-md-3'>
             <AdminMenu />
           </div>
-          <div className='col-md-9'>content</div>
+          <div className='col-md-9'>
+            <div className='card w-75 p-3'>
+              <h3>Admin Name: {auth?.user?.name}</h3>
+              <h3>Admin Email: {auth?.user?.email}</h3>
+              <h3>Admin Contact: {auth?.user?.phone}</h3>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
