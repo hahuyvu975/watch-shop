c94ffdb158be4a7ef1e793dd3a85dbe13d9874bd
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-07 16:57:02 +0700

    create menu for user

---
+++ client/src/components/Layout/UserMenu.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const UserMenu = () => {
+    return (
+        <>
+            <div className='text-center'>
+                <div className="list-group">
+                    <h4>User DashBoard</h4>
+                    <NavLink to="/dashboard/user/profile" className="list-group-item">
+                        Profile
+                    </NavLink>
+                    <NavLink to="/dashboard/user/orders" className="list-group-item">
+                        Orders
+                    </NavLink>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default UserMenu
