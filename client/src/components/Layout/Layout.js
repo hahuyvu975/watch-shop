2cdb7cdcbf14db5965a6e1d21ca726f45bb53160
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-07 07:17:42 +0700

    rm toast

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import { Helmet } from 'react-helmet';
-import { Toaster} from 'react-hot-toast'
 
 const Layout = ({ children, title, description, keywords, author }) => {
     return (
@@ -16,7 +15,6 @@ const Layout = ({ children, title, description, keywords, author }) => {
             </Helmet>
             <Header />
             <main style={{ minHeight: '70vh' }}>
-                <Toaster />
                 {children}
             </main>
             <Footer />
