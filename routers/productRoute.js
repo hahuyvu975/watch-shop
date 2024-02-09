e1b448a1f0d5e30de1fea7113542d9397edf6702
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-09 18:58:44 +0700

    router get product photo

--- routers/productRoute.js
+++ routers/productRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
-import { createProductController, getAllProductController } from '../controllers/productController.js';
+import { createProductController, getAllProductController, getSingleProductController, productPhotoController } from '../controllers/productController.js';
 import formidable from 'express-formidable';
 const router = express.Router();
 
@@ -11,5 +11,11 @@ router.post('/create-product', requireSignIn, isAdmin, formidable(), createProdu
 
 //get all product
 router.get('/get-product', requireSignIn, isAdmin, getAllProductController);
+
+//get single product
+router.get('/single-product/:slug', requireSignIn, isAdmin, getSingleProductController)
+
+//get photo
+router.get('/product-photo/:pid', requireSignIn, isAdmin, productPhotoController)
 
 export default router;
