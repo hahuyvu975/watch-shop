9621c3a28e3f97d476fc82487c51d5fb8574461a
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-10 03:48:02 +0700

    get data api and layout table

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout/Layout'
 import AdminMenu from '../../components/Layout/AdminMenu'
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const CreateCategory = () => {
+    const [categories, setCategories] = useState([]);
+    const navigate = useNavigate();
+
+    //get all categories
+    const getAllCategories = async () => {
+        try {
+            const { data } = await axios.get('/api/v1/admin/category/get-category');
+            console.log(data.categories[0]);
+            if (data.success) {
+                setCategories(data.categories);
+            }
+        } catch (error) {
+            toast.error("Error: Show list categories")
+        }
+    };
+
+    useEffect(() => {
+        getAllCategories();
+    }, []);
+
+    const handleCategoryForm = () => {
+        navigate('/api/v1/admin/update-category');
+    }
+
     return (
         <Layout title={"Dashboard - Create Category"}>
             <div className='container-fluid m-3 p-3'>
@@ -11,7 +38,27 @@ const CreateCategory = () => {
                         <AdminMenu />
                     </div>
                     <div className='col-md-9'>
-                        <h1>Create Category</h1>
+                        <h1>Manage Category</h1>
+                        <div className="w-75">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {categories?.map((c) => (
+                                            <tr key={c._id}>
+                                                <td>{c.name}</td>
+                                                <td>
+                                                    <button className='btn btn-primary' onClick={handleCategoryForm}>Edit</button>
+                                                </td>
+                                            </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
