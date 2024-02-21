e9b81165e21f927971077bca9bacd0b03a749168
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-21 15:52:51 +0700

    update product

--- client/src/pages/Admin/UpdateProduct.js
+++ client/src/pages/Admin/UpdateProduct.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import Layout from '../../components/Layout/Layout';
-import AdminMenu from '../../components/Layout/AdminMenu';
-import { toast } from 'react-hot-toast';
-import axios from 'axios';
-import { Select } from 'antd';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Layout from "./../../components/Layout/Layout";
+import AdminMenu from "./../../components/Layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { Select } from "antd";
+import { useNavigate, useParams } from "react-router-dom";
 const { Option } = Select;
 
 const UpdateProduct = () => {
-    const [categories, setCategories] = ([]);
+    const navigate = useNavigate();
+    const params = useParams();
+    const [categories, setCategories] = useState([]);
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     const [price, setPrice] = useState("");
@@ -18,29 +20,49 @@ const UpdateProduct = () => {
     const [photo, setPhoto] = useState("");
     const [id, setId] = useState("");
 
-    const navigate = useNavigate();
-    const params = useParams();
-
     //get single product
-    const handleGetDataProduct = async () => {
+    const getSingleProduct = async () => {
         try {
-            const { data } = await axios.get(`/api/v1/admin/product/single-product/${params.slug}`)
-            setId(data.product._id);
+            const { data } = await axios.get(
+                `/api/v1/admin/product/single-product/${params.slug}`
+            );
+
             setName(data.product.name);
+            setId(data.product._id);
             setDescription(data.product.description);
             setPrice(data.product.price);
+            setPrice(data.product.price);
             setQuantity(data.product.quantity);
-            setCategory(data.product.category);
             setShipping(data.product.shipping);
-
+            setCategory(data.product.category._id);
+            console.log(data.product._id)
         } catch (error) {
-            console.log(error.message);
-            toast.error('Erron in get single product');
+            console.log(error);
         }
-    }
+    };
+    useEffect(() => {
+        getSingleProduct();
+    }, []);
 
-    // update product
-    const handleUpdateProduct = async (e) => {
+    //get all category
+    const getAllCategory = async () => {
+        try {
+            const { data } = await axios.get("/api/v1/admin/category/get-category");
+            if (data?.success) {
+                setCategories(data?.categories);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Something wwent wrong in getting catgeory");
+        }
+    };
+
+    useEffect(() => {
+        getAllCategory();
+    }, []);
+
+    //create product function
+    const handleUpdate = async (e) => {
         e.preventDefault();
         try {
             const productData = new FormData();
@@ -50,131 +72,143 @@ const UpdateProduct = () => {
             productData.append("quantity", quantity);
             photo && productData.append("photo", photo);
             productData.append("category", category);
-            const { data } = await axios.post('/api/v1/admin/product/update-product',
+            const { data } = await axios.put(
+                `/api/v1/admin/product/update-product/${id}`,
                 productData
             );
-
-            console.log(data)
             if (data?.success) {
-                toast.success(data?.message);
-                navigate('/dashboard/admin/products');
+                toast.success("Product Updated Successfully");
+                navigate("/dashboard/admin/products");
             } else {
                 toast.error(data?.message);
             }
         } catch (error) {
             console.log(error);
-            toast.error("Error in create product")
+            toast.error("something went wrong");
         }
     };
 
-    useEffect(() => {
-        handleGetDataProduct();
-    }, [])
+    //delete a product
+    const handleDelete = async () => {
+        try {
+            let answer = window.prompt("Are You Sure want to delete this product ? ");
+            if (!answer) return;
+            const { data } = await axios.delete(
+                `/api/v1/admin/product/delete-product/${id}`
+            );
+            toast.success("Product deleted Succfully");
+            navigate("/dashboard/admin/products");
+        } catch (error) {
+            console.log(error);
+            toast.error("Something went wrong");
+        }
+    };
     return (
-        <Layout title={"Dashboard - Update Product"}>
-            <div className='container-fluid m-3 p-3'>
-                <div className='row'>
-                    <div className='col-md-3'>
+        <Layout title={"Dashboard - Create Product"}>
+            <div className="container-fluid m-3 p-3">
+                <div className="row">
+                    <div className="col-md-3">
                         <AdminMenu />
                     </div>
-                    <div className='col-md-9'>
+                    <div className="col-md-9">
                         <h1>Update Product</h1>
-                        <div className='m-1 w-75'>
+                        <div className="m-1 w-75">
                             <Select
                                 variant={false}
                                 placeholder="Select a category"
                                 size="large"
                                 showSearch
-                                className='form-select mb-3'
+                                className="form-select mb-3"
                                 onChange={(value) => {
-                                    setCategory(value)
+                                    setCategory(value);
                                 }}
-                                value={category.name}
+                                value={category}
                             >
-                                {categories?.map(c => (
+                                {categories?.map((c) => (
                                     <Option key={c._id} value={c._id}>
                                         {c.name}
                                     </Option>
                                 ))}
                             </Select>
-                            <div className='mb-3'>
-                                <label className='btn btn-outline-secondary col-md-12'>
+                            <div className="mb-3">
+                                <label className="btn btn-outline-secondary col-md-12">
                                     {photo ? photo.name : "Upload Photo"}
                                     <input
-                                        type='file'
-                                        name='photo'
-                                        accept='image/*'
+                                        type="file"
+                                        name="photo"
+                                        accept="image/*"
                                         onChange={(e) => setPhoto(e.target.files[0])}
                                         hidden
                                     />
                                 </label>
                             </div>
-                            <div className='mb-3'>
+                            <div className="mb-3">
                                 {photo ? (
-                                    <div className='text-center'>
+                                    <div className="text-center">
                                         <img
                                             src={URL.createObjectURL(photo)}
-                                            alt='product_photo'
+                                            alt="product_photo"
                                             height={"200px"}
-                                            className='img img-responsive'
+                                            className="img img-responsive"
                                         />
                                     </div>
                                 ) : (
-                                    <div className='text-center'>
+                                    <div className="text-center">
                                         <img
                                             src={`/api/v1/admin/product/product-photo/${id}`}
-                                            alt='product_photo'
+                                            alt="product_photo"
                                             height={"200px"}
-                                            className='img img-responsive'
+                                            className="img img-responsive"
                                         />
                                     </div>
                                 )}
                             </div>
-                            <div className='mb-3'>
+                            <div className="mb-3">
                                 <input
-                                    type='text'
+                                    type="text"
                                     value={name}
-                                    placeholder='write a name'
-                                    className='form-control'
+                                    placeholder="write a name"
+                                    className="form-control"
                                     onChange={(e) => setName(e.target.value)}
                                 />
                             </div>
-                            <div className='mb-3'>
+                            <div className="mb-3">
                                 <textarea
-                                    type='text'
+                                    type="text"
                                     value={description}
-                                    placeholder='write a description'
-                                    className='form-control'
+                                    placeholder="write a description"
+                                    className="form-control"
                                     onChange={(e) => setDescription(e.target.value)}
                                 />
                             </div>
-                            <div className='mb-3'>
+
+                            <div className="mb-3">
                                 <input
-                                    type='number'
+                                    type="number"
                                     value={price}
-                                    placeholder='write a price'
-                                    className='form-control'
+                                    placeholder="write a Price"
+                                    className="form-control"
                                     onChange={(e) => setPrice(e.target.value)}
                                 />
                             </div>
-                            <div className='mb-3'>
+                            <div className="mb-3">
                                 <input
-                                    type='number'
+                                    type="number"
                                     value={quantity}
-                                    placeholder='write a quantity'
-                                    className='form-control'
+                                    placeholder="write a quantity"
+                                    className="form-control"
                                     onChange={(e) => setQuantity(e.target.value)}
                                 />
                             </div>
-                            <div className='mb-3'>
+                            <div className="mb-3">
                                 <Select
                                     variant={false}
-                                    placeholder='Select Shipping'
-                                    size='large'
+                                    placeholder="Select Shipping "
+                                    size="large"
                                     showSearch
-                                    className='form-select mb-3'
-                                    onChange={(e) => {
-                                        setShipping(e)
+                                    className="form-select mb-3"
+                                    onChange={(value) => {
+                                        setShipping(value);
                                     }}
                                     value={shipping ? "Yes" : "No"}
                                 >
@@ -182,9 +216,14 @@ const UpdateProduct = () => {
                                     <Option value="1">Yes</Option>
                                 </Select>
                             </div>
-                            <div className='mb-3'>
-                                <button className='btn btn-primary' onClick={handleUpdateProduct}>
+                            <div className="mb-3">
+                                <button className="btn btn-primary" onClick={handleUpdate}>
                                     UPDATE PRODUCT
+                                </button>
+                            </div>
+                            <div className="mb-3">
+                                <button className="btn btn-danger" onClick={handleDelete}>
+                                    DELETE PRODUCT
                                 </button>
                             </div>
                         </div>
@@ -192,7 +231,7 @@ const UpdateProduct = () => {
                 </div>
             </div>
         </Layout>
-    )
-}
+    );
+};
 
-export default UpdateProduct
+export default UpdateProduct;
