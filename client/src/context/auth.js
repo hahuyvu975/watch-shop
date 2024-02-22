24543f7f158713fc2b189bf4f641a32ea2df8a09
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-22 17:46:14 +0700

    fix auth

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,36 +1,36 @@
-    import axios from "axios";
-    import { useContext, useState, useEffect, createContext } from "react";
+import axios from "axios";
+import { useContext, useState, useEffect, createContext } from "react";
 
-    const AuthContext = createContext();
+const AuthContext = createContext();
 
-    const AuthProvider = ({ children }) => {
-        const [auth, setAuth] = useState({
-            user: null,
-            token: ""
-        });
+const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useState({
+        user: null,
+        token: ""
+    });
 
-        //default axios
-        axios.defaults.headers.common["Authorization"] = auth?.token
+    //default axios
+    axios.defaults.headers.common["Authorization"] = auth?.token
 
-        useEffect(() => {
-            const data = localStorage.getItem("auth");
-            if (data) {
-                const parseData = JSON.parse(data);
-                setAuth({
-                    ...auth,
-                    user: parseData.user,
-                    token: parseData.token
-                });
-            }
-        }, []);
-        return (
-            <AuthContext.Provider value={[auth, setAuth]}>
-                {children}
-            </AuthContext.Provider>
-        );
-    };
+    useEffect(() => {
+        const data = localStorage.getItem("auth");
+        if (data) {
+            const parseData = JSON.parse(data);
+            setAuth({
+                ...auth,
+                user: parseData.user,
+                token: parseData.token
+            });
+        }
+    }, []);
+    return (
+        <AuthContext.Provider value={[auth, setAuth]}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
 
-    //custom hook
-    const useAuth = () => useContext(AuthContext);
+//custom hook
+const useAuth = () => useContext(AuthContext);
 
-    export { useAuth, AuthProvider }
+export { useAuth, AuthProvider }
