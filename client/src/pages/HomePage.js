1c2b8f02ab60400df1728f53c01f1407308a7a83
hahuyvu975 <hahuyvu975@gmail.com>
2024-02-28 12:05:57 +0700

    feature: filter product by checkbox and radio

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout/Layout';
-import { useAuth } from '../context/auth.js';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
-import { Link } from 'react-router-dom';
+import { Checkbox, Radio } from 'antd';
+import { Prices } from '../components/Prices';
+
 const HomePage = () => {
-    const [auth, setAuth] = useAuth();
     const [products, setProducts] = useState([]);
     const [categories, setCategories] = useState([]);
+    const [checked, setChecked] = useState([]);
+    const [radio, setRadio] = useState([]);
+
+    //get all categories
+    const getAllCategories = async () => {
+        try {
+            const { data } = await axios.get('/api/v1/admin/category/get-category');
+            if (data?.success) {
+                setCategories(data?.categories);
+            } else {
+                toast.error(data?.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Error in handle get all categoris");
+        }
+    };
+
+    useEffect(() => {
+        getAllCategories();
+    },[]);
 
     // get all products
     const getAllProducts = async () => {
@@ -23,34 +44,92 @@ const HomePage = () => {
             console.log(error);
             toast.error('Error in get all products')
         }
+    };
+
+    useEffect(() => {
+        if (!checked.length || !radio.length) {
+            getAllProducts();
+        };
+    }, [checked.length, radio.length]);
+
+    //filter product by category
+    const handleFilterProduct = (value, id) => {
+        let all = [...checked];
+        if (value) {
+            all.push(id);
+        } else {
+            all = all.filter((c) => c !== id);;
+        }
+        setChecked(all);
+    };
+
+    //get filter product
+    const filterProduct = async () => {
+        try {
+            const { data } = await axios.post('/api/v1/admin/product/product-filter', { checked, radio })
+            console.log('123', data)
+            if (data?.success) {
+                setProducts(data?.products)
+            } else {
+                toast.error(data?.message)
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error("Error in handle filter product");
+        }
     }
 
     useEffect(() => {
-        getAllProducts();
-    }, [])
+        if (checked.length || radio.length) {
+            filterProduct();
+        }
+    }, [checked, radio])
+
     return (
         <Layout title={"All Products - Best offers"}>
-            <div className='row mt-3'>
+            <div className='row m-3'>
                 <div className='col-md-3'>
                     <h4 className='text-center'>Filter By Category</h4>
+                    <div className='d-flex flex-column'>
+                        {categories?.map((c) => (
+                            <Checkbox key={c._id} onChange={(e) => handleFilterProduct(e.target.checked, c._id)}>
+                                {c.name}
+                            </Checkbox>
+                        ))}
+                    </div>
+                    <h4 className='text-center mt-4'>Filter By Price</h4>
+                    <div className='d-flex flex-column'>
+                        <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                            {Prices?.map((p) => (
+                                <div key={p._id}>
+                                    <Radio value={p.array}>{p.name}</Radio>
+                                </div>
+                            ))}
+                        </Radio.Group>
+                    </div>
                 </div>
                 <div className='col-md-9'>
                     <h1 className='text-center'>All Products</h1>
                     <div className='d-flex flex-wrap'>
-                        {products?.map((p) => (           
-                                <div key={p._id}  className="card m-2" style={{ width: "18rem" }}>
-                                    <img
-                                        src={`/api/v1/admin/product/product-photo/${p._id}`}
-                                        className="card-img-top"
-                                        alt={p.name}
-                                        style={{ width: "100%", height: "300px" }}
+                        {products?.map((p) => (
+                            <div key={p._id} className="card m-2" style={{ width: "18rem" }}>
+                                <img
+                                    src={`/api/v1/admin/product/product-photo/${p._id}`}
+                                    className="card-img-top"
+                                    alt={p.name}
+                                    style={{ width: "100%", height: "300px" }}
 
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">{p.name}</h5>
-                                        <p className="card-text">{p.description}</p>
-                                    </div>
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">{p.name}</h5>
+                                    <p className="card-text">{p.description.length > 30 ?
+                                        p.description.substring(0,30) + "..." : p.description}</p>
+                                    <p className="card-text">$ {p.price}</p>
+                                    <button className='btn btn-primary ms-1'>More details</button>
+                                    <button className='btn btn-secondary ms-1'>Add to cart</button>
                                 </div>
+                            </div>
                         ))}
                     </div>
                 </div>
